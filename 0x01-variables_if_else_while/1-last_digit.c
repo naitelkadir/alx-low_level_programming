bfc3f7e8c67b359ac017c01c2cbe5fe4bb4cfc6f
naitelkadir <naitelkadir@gmail.com>
2023-02-17 03:26:16 -0800

    add task 1 update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,8 +19,8 @@ int main (void)
 	
 	if (a > 5)
 		printf("%s %d is %d and is greater than 5\n", str, n, a);
-	else if (a==0)
-		printf("%s %d is %d and and is 0\n", str, n, a);
+	else if (a == 0)
+		printf("%s %d is %d and is 0\n", str, n, a);
 	else if ( a < 6)
 		printf("%s %d is %d and is less than 6 and not 0\n", str, n, a);
 	return (0);
