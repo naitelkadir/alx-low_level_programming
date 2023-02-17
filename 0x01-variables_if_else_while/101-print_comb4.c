d068c373fd04d8c6a1c7661ae90fff254e52c767
naitelkadir <naitelkadir@gmail.com>
2023-02-17 12:25:46 -0800

    tt

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+	int a;
+	int b;
+	int c;
+	for (c = 0; c < 10; c++)
+	{
+		putchar('0' + c);
+		for (d = 0; d < 90; d++)
+		{
+			a = d / 10;
+			b = d % 10;
+			if (d != 89)
+			{
+				if (a < b)
+				{
+					putchar('0' + a);
+					putchar('0' + b);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			else
+			{
+				putchar('0' + a);
+				printf("%d\n", b);
+			}
+		}
+	}
+	return (0);
+}
