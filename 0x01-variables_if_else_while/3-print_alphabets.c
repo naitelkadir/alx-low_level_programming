f5f9c0b8eeb0136983abbcf11d5bdb88c321c507
naitelkadir <naitelkadir@gmail.com>
2023-02-17 05:10:42 -0800

    tt

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-	char ch;
+	char low;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
-	printf("\n");
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+		putchar('\n');
 
 	return (0);
 }	
