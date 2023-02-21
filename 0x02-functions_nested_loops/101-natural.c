c43d20b393f70705a0c60b5c3cbaa2b8a13552d5
naitelkadir <naitelkadir@gmail.com>
2023-02-21 09:40:16 -0800

    hh

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,10 +13,10 @@ int main(void)
 	s = 0;
 	for (i = 0; i < 1024; i++)
 	{
-			if ((i % 3 == 0) || (i % 5 == 0))
-			{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
 				s = s + i;
-			}
+		}	
 	}
 	printf("%d\n", s);
 	return (0);
