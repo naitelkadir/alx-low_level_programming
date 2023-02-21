459c1a04f846693d4f88be4877ad112cea26e90f
naitelkadir <naitelkadir@gmail.com>
2023-02-21 14:52:23 -0800

    hh

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: none
+ */
+
+int main(void)
+{
+	
+	int a = 0;
+	long b = 1, c = 2;
+
+	while (i < 50)
+	{
+		if (a == 0)
+		{
+			printf("%ld", b);
+		}
+		else if (a == 1)
+		{
+			printf(", %ld", c);
+		}
+		else
+		{
+			c += b;
+			b = c - b;
+			printf(", %ld", c);
+		}
+
+		++a;
+	}
+
+	printf("\n");
+	return (0);
+}
