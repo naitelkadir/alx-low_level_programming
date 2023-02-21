7e1a459c4d5f11c0907ffa79397bc75eb58b4993
naitelkadir <naitelkadir@gmail.com>
2023-02-21 07:29:54 -0800

    tt

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,25 +10,25 @@ void times_table(void)
 	
 	for (a = 0; a <= 9; a++)
 	{
-		putchar('0');
-		putchar(',');
-		putchar(' ');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		for (b = 1; b <= 9; b++)
 		{
 			c = a * b;
 			if ((c / 10) > 0)
 			{
-				putchar('0' + c / 10);
+				_putchar('0' + c / 10);
 			}
 			else
 			{
-				putchar(' ');
+				_putchar(' ');
 			}
-			putchar('0' + (c % 10));
+			_putchar('0' + (c % 10));
 			if (b < 9)
-				putchar(',');
-				putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
