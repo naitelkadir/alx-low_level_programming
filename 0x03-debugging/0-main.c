1c3891a62141c5105d2e0f2024428644660a9ac8
naitelkadir <naitelkadir@gmail.com>
2023-02-23 02:56:47 -0800

    hh

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative in the case of 0
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
