8d466fddff1442cd7b8324cc2330a2f6775ef891
naitelkadir <naitelkadir@gmail.com>
2023-02-27 09:39:59 -0800

    hh

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,18 +8,14 @@
  */
 void puts2(char *str)
 {	
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (; str[i] != '\0'; i++)
 	{
-		if (i % 2 == 0)
-		{
+		if ((i % 2) == 0)
 			_putchar(str[i]);
-		}
 		else
-		{
 			continue;
-		}
 	}
 	_putchar('\n');
 }
