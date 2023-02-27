baa2e2b02f7dc0c55db0131ab179f69d80659442
naitelkadir <naitelkadir@gmail.com>
2023-02-27 11:09:21 -0800

    hhh

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,10 +10,10 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n  ; i++)
 	{
-		if ( i != (n - 1))
+		if (i != (n - 1))
 		{
 			printf("%d, ", a[i]);
 		}
@@ -21,7 +21,6 @@ void print_array(int *a, int n)
 		{
 			printf("%d", a[n - 1]);
 		}
-		
 	}
 	printf("\n");
 }
