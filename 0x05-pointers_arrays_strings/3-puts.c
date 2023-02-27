df72e8fabc6ff0a13aca535ff10ba98725cdc552
naitelkadir <naitelkadir@gmail.com>
2023-02-27 06:35:38 -0800

    add task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
+ */
+void _puts(char *str)
+{
+	int i;
+	char ch;
+	
+	i = 1;
+	ch = str[0];
+	
+	while (ch != '\0')
+	{
+		_pucthar(ch);
+		ch = str[i++];
+	}
+	_putchar('\n');
+}
