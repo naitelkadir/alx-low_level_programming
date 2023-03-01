b310de82b3ce0c2bf3f8fe7a8f844b640dbf06ff
naitelkadir <naitelkadir@gmail.com>
2023-03-01 11:32:34 -0800

    hh

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,9 +16,11 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 		;
 
-	for (j = 0; src[j] != '\0' && n > 0; j++; n--; i++)
+	for (j = 0; src[j] != '\0' && n > 0; j++)
 	{
 		dest[i] = src[c];
+		i++;
+		n--;
 	}
 	return (dest);
 }
