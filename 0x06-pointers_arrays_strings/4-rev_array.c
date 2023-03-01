58f6700aae7cbdd231990c588367e4ad1d608410
naitelkadir <naitelkadir@gmail.com>
2023-03-01 05:57:32 -0800

    add task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - a function that reverses the content of an array of int
+ * @a: content
+ * @n: elements of content
+ * Return: Always 0 (Success)
+ */
+void reverse_array(int *a, int n)
+{
+	int i, rev;
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		rev = a[i];
+		a[i] = a[n];
+		a[n] = rev;
+	}
+	
+}
