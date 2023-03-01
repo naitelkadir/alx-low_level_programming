babbc3aa145cbc4ba247969b299577c1699dee48
naitelkadir <naitelkadir@gmail.com>
2023-03-01 15:15:52 -0800

    hh

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -17,7 +17,7 @@ void print_number(int n)
                 _putchar('-');
         }
 	k /= 10;
-	 if (k != 0)
+	if (k != 0)
 		 print_number(k);
-	 _putchar((unsigned int) n % 10 + '0');
+	 _putchar(n % 10 + '0');
 }
