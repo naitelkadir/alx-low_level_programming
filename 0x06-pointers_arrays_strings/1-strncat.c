98764129a17e0d3b29c34457fd1bb46e91fbb363
naitelkadir <naitelkadir@gmail.com>
2023-03-01 11:46:43 -0800

    hh

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,18 +8,19 @@
  * Return: Always 0 (Success)
  */
 
-
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i, n;
-
+	int i, j;
+	
 	for (i = 0; dest[i] != '\0'; i++)
 	{
+	
 	}
-
-	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	for (j = 0;src[j] != '\0' && j < n; j++)
 	{
+		dest[i] = src[j];
+		i++;
 	}
-
+	dest[i] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, c;
-	
-	c = 0;
+	int _strcmp(char *s1, char *s2)
+{
+	int i;
+
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
-		{
-			c = c + (s1[i] - s2[i]);
-			break;
-		}
+			return (s1[i] - s2[i]);
 	}
-	return (c);
+	return (0);
 }
