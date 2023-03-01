353c84af8ec45e8ac49e5ffdf420927d2ce77456
naitelkadir <naitelkadir@gmail.com>
2023-03-01 12:46:50 -0800

    hh

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,8 +10,7 @@ char *cap_string(char *n)
 {
 	int i, x;
 	int cap = 32;
-	int separators[] = {',', ';', '.', '?', '"',
-		 '(', ')', '{', '}', ' ', '\n', '\t'};
+	int separators[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
