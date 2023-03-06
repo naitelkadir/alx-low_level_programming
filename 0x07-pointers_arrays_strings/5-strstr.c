31b1d0a3bf46aabd8fd9fc1200dcbf23e317c94c
naitelkadir <naitelkadir@gmail.com>
2023-03-06 06:49:57 -0800

    hh

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,20 +14,17 @@ char *_strstr(char *haystack, char *needle)
 	
 	i = 0;
 	while (haystack[i] != '\0')
-    	{
-		if (haystack[i] == needle[0])
+	{
+ 		for (c = 1; needle[c] != '\0'; c++)
 		{
-			for (c = 1; needle[c] != '\0'; c++)
+			if (haystack[i + c] != needle[c])
 			{
-				if (haystack[i + c] != needle[c])
-				{
-					break;
-				}
+				break;
 			}
-			if (needle[c] == '\0')
-			{
-				return (haystack + i);
-			}
+		}
+		if (needle[c] == '\0')
+		{
+			return (haystack + i);
 		}
 		i++;
 	}
