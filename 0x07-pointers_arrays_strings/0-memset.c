0a7ae8cdf01d1f0624f0fec03a4880d9acacb9d5
naitelkadir <naitelkadir@gmail.com>
2023-03-06 03:59:41 -0800

    hh

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /*
@@ -8,10 +7,11 @@
  * @n: bytes
  * Return: Always 0 (Success)
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
