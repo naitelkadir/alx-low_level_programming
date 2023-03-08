d642438321a174bae224227f5a3549bad9a9de20
naitelkadir <naitelkadir@gmail.com>
2023-03-08 02:26:32 -0800

    hh

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,17 +7,17 @@
   * Return: integer value
   */
 int factorial(int n)	
-{
-	if ( n < 0)
+{	
+	if (n < 0)
 	{
 		return (-1);
 	}
-	else if ( n == 0)
+	else if (n == 0)
 	{
 		return (1);
 	}
 	else
 	{
-		return n * factorial (n - 1);
+		return (n * factorial (n - 1));
 	}
 }
