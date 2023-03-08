d68c84edbed6a5df0611a072f0e0bae926969dee
naitelkadir <naitelkadir@gmail.com>
2023-03-08 14:17:23 -0800

    hh

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int isPalRec(char *s,int i, int e);
+int isPalRec(char *s, int i, int e);
 int _strlen_recursion(char *s);
 
 /**
@@ -12,12 +12,11 @@ int _strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 	int n = _strlen_recursion(s);
-     
+
 	if (n == 0)
 		return (1);
-     	return isPalRec(s, 0, n - 1);
+	return (isPalRec(s, 0, n - 1));
 }
-
 /**
   * isPalRec - Check if a string is palindrome
   * @s: the string value to be checked
@@ -25,18 +24,16 @@ int is_palindrome(char *s)
   * @e: second index
   * Return: integer value
   */
-
-int isPalRec(char *s,int i, int e)
+int isPalRec(char *s, int i, int e)
 {
 	if (i == e)
 		return (1);
 	if (s[i] != s[e])
 		return (0);
 	if (i < e + 1)
-		return isPalRec(s, i + 1, e - 1);
+		return (isPalRec(s, i + 1, e - 1));
 	return (1);
 }
-
 /**
   * _strlen_recursion - Get the length of a string
   * @s: the string to get the length
