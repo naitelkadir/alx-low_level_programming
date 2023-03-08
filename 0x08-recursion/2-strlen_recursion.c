0cfd7c5d69232b705bbb6469d543605464662244
naitelkadir <naitelkadir@gmail.com>
2023-03-08 01:53:32 -0800

    hh

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,9 +8,10 @@
   */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		s = s + 1;
-		return (1 + _strlen_recursion(s));
+		return (0);
 	}
+	s = s + 1;
+	return (1 + _strlen_recursion(s));
 }
