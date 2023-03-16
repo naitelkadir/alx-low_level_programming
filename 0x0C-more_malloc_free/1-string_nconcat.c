8b2cf2dc8686925706fb0b465cc0ad82a90b541a
naitelkadir <naitelkadir@gmail.com>
2023-03-16 06:21:48 -0700

    ghg

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,10 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str, *empt;
-	int i, j, a;
+	int i, j, a, b;
 	
 	a = 0;
+	b = 0;
 	empt = "";
 	if (s1 == NULL)
 	{
@@ -28,8 +29,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		a++;
 	}
+	while (s2[b] != '\0')
+	{
+		b++;
+	}
+	if (b <= n)
+	{
+		n = b;
+	}
 	str = malloc((a + n + 1) * sizeof(char *));
-	if ( str == NULL)
+	if (str == NULL)
 	{
 		return (NULL);
 	}
@@ -37,7 +46,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		str[i] = s1[i];
 	}
-	for (j = 0; j < n & s2[j] != '\0'; j++)
+	for (j = 0; j < n; j++)
 	{
 		str[a + j] = s2[j];
 	}
