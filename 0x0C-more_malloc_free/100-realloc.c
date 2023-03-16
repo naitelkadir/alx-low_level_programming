3dd1743a594d400fc95ac3b8b305d2c0c19c68c3
naitelkadir <naitelkadir@gmail.com>
2023-03-16 14:58:07 -0700

    hh

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _realloc - reallocates old to new, set conditions from problem
+ * returning dest w/ size of malloc new_size, set src as ptr
+ * @ptr: pointer to memory prev alloc, must free end
+ * @old_size: input old
+ * @new_size: input new
+ * Return: 0
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *str;
+	unsigned int i;
+	
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		return (ptr);
+	}
+	str = malloc(new_size);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < new_size && i < old_size; i++)
+	{
+		str[i] = ptr[i];
+	}
+	return (str);
+}
