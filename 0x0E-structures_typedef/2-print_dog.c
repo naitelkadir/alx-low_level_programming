4554216e421098c1f13bcdd33ac5915654ae8a0e
naitelkadir <naitelkadir@gmail.com>
2023-03-20 03:27:11 -0700

    hh

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,15 +9,22 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
+	{
 		return;
+	}
 	if (d->name == NULL)
+	{
 		d->name = "(nil)";
+	}
 	if (d->age <= 0)
-		printf("Age: (nil)\n");
+	{
+		d->age = "(nil)";
+	}
 	if (d->owner == NULL)
+	{
 		d->owner = "(nil)";
-
+	}
 	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
+	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
