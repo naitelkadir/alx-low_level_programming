d184af4a404a851f95148d099053531a6556ec99
naitelkadir <naitelkadir@gmail.com>
2023-03-22 07:30:27 -0700

    add task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,15 +14,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	
 	if (size <= 0)
 	{
-		return (-1);
+		return (- 1);
 	}
 	if (array == NULL)
 	{
-		return (-1);
+		return (- 1);
 	}
 	if (cmp == NULL)
 	{
-		return (-1);
+		return (- 1);
 	}
 	for (i = 0; i < size; i++)
 	{
@@ -31,5 +31,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 			return (i);
 		}
 	}
-	return (-1);
+	return (- 1);
 }
