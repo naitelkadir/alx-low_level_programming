5150a79810c9551602e2ae9fa304f90fceaeb60e
naitelkadir <naitelkadir@gmail.com>
2023-03-22 07:08:04 -0700

    hh

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,17 +11,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-	
+
 	if (array == NULL)
-	{
 		return;
-	}
 	if (action == NULL)
-	{
 		return;
-	}
+
 	for (i = 0; i < size; i++)
-	{
-		action(arr[i]);
-	}
+		action(array[i]);
 }
