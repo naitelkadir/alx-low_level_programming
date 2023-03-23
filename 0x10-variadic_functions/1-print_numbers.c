2703d51772fd02113e66fca3e0bd792d1717c15c
naitelkadir <naitelkadir@gmail.com>
2023-03-23 07:00:48 -0700

    hh

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,27 +4,27 @@
 /**
  * print_numbers - Entry Point
  * @separator: comma space
- * @n: numbers of parameters
+ * @n: elements to be printed
  * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	
+
 	if (separator == NULL)
-	{
 		return;
-	}
+
 	va_start(ap, n);
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, const unsigned int));
-		if(i != n - 1)
-		{
+
+		if (i != (n - 1))
 			printf("%s", separator);
-		}
 	}
 	printf("\n");
+
 	va_end(ap);
 }
