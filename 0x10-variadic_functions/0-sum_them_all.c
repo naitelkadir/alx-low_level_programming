828cf90feee93d15ef2964e39382ecd510b1728e
naitelkadir <naitelkadir@gmail.com>
2023-03-23 06:26:20 -0700

    last test

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,22 +9,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, s = 0;
 	va_list ap;
-	
+	unsigned int i, sum = 0;
+
 	if (n == 0)
-	{
 		return (0);
-	}
-	
-	va_start(ap,n);
-	
+
+	va_start(ap, n);
+
 	for (i = 0; i < n; i++)
 	{
-		s = s + va_arg(ap, const unsigned int);
+		sum += va_arg(ap, const unsigned int);
 	}
-	
+
 	va_end(ap);
-	
-	return (s);
+
+	return (sum);
 }
