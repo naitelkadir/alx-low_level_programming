1a662d8113e541791431e622857a08010e0ff3bf
naitelkadir <naitelkadir@gmail.com>
2023-03-30 04:14:11 -0700

    create len_list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *list_len - Return the number of elements in a linked list
+ *@h: linked list
+ *
+ *Return: the number of node
+ */
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+count++;
+h = h -> next;
+}
+return (count);
+}
