ddb12170578cdfdea3c02ac9cf253c0b7713a562
naitelkadir <naitelkadir@gmail.com>
2023-04-03 09:50:28 -0700

    up

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,19 +5,19 @@
  * print_listint - Prints all elements of a liked list
  * @h: Linked list
  *
- * Return : The numne of nodes
+ * Return : The number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-int c = 0;
-if (h != NULL)
-{
-while (h != NULL)
-{
-printf("%d\n", h->n);
-c++;
-h = h->next;
-}
-}
-return (c);
+	int c = 0;
+	if (h != NULL)
+	{	
+		while (h != NULL)
+		{
+			printf("%d\n", h->n);
+			c++;
+			h = h->next;
+		}
+	}
+	return (c);
 }
