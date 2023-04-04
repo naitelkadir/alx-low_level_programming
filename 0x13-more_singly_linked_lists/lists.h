ed963f80220821af3bffef885cee843859c722ef
naitelkadir <naitelkadir@gmail.com>
2023-04-04 06:49:58 -0700

    up

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -29,5 +28,4 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
-#endif /* LISTS_H */
+#endif
