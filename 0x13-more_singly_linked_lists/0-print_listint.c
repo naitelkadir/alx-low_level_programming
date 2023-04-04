b3d4f287fd675c5c7d40c632cecaa89a35e2eb14
naitelkadir <naitelkadir@gmail.com>
2023-04-03 18:23:01 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,13 +4,11 @@
 /**
  * print_listint - Prints all elements of a liked list
  * @h: Linked list
- *
  * Return : The number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-	int c = 0;
+	size_t c = 0;
 	if (h != NULL)
 	{	
 		while (h != NULL)
