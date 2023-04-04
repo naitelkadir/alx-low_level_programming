4c2091b96cd7d4015e26827151cd04013f3ca89b
naitelkadir <naitelkadir@gmail.com>
2023-04-03 18:29:35 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t c = 0;
-	if (h != NULL)
-	{	
-		while (h != NULL)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			c++;
-		}
-	}
-	return (c);
+size_t c = 0;	
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+c++;
+}
+return (c);
 }
