35a8f0f97665ead317e8acf701d332b56f0696ac
naitelkadir <naitelkadir@gmail.com>
2023-04-04 05:50:10 -0700

    test

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	else
 	{
 		tmp = *head;
-		for (i = 1; i < index; i++)
+		for (i = 2; i < index; i++)
 		{
 			if (tmp == NULL)
 			{
