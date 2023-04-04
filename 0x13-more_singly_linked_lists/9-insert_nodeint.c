ad2fb16cd981e164bb85a62b4ca649766569ad5c
naitelkadir <naitelkadir@gmail.com>
2023-04-04 10:30:14 -0700

    fix betty style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,39 +12,39 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *newnode;
-	listint_t *tmp;
+unsigned int i;
+listint_t *newnode;
+listint_t *tmp;
 
 if (head != NULL)
 {
-	newnode = malloc(sizeof(listint_t));
-	if (newnode == NULL)
-	{
-		return (NULL);
-	}
-	newnode->n = n;
-	if (idx <= 0)
-	{
-		newnode->next = *head;
-                *head = newnode;
-		return (*head);
-	}
-	else
-	{
-		tmp = *head;
-		for (i = 1; i < idx; i++)
-		{
-			if (tmp == NULL)
-			{
-				return (NULL);
-			}
-			tmp = tmp->next;
-		}
-		newnode->next = tmp->next;
-		tmp->next = newnode;
-		return (tmp);
-	}
+newnode = malloc(sizeof(listint_t));
+if (newnode == NULL)
+{
+return (NULL);
+}
+newnode->n = n;
+if (idx <= 0)
+{
+newnode->next = *head;
+*head = newnode;
+return (*head);
+}
+else
+{
+tmp = *head;
+for (i = 1; i < idx; i++)
+{
+if (tmp == NULL)
+{
+return (NULL);
+}
+tmp = tmp->next;
+}
+newnode->next = tmp->next;
+tmp->next = newnode;
+return (tmp);
+}
 }
 return (NULL);
 }
