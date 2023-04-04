d32d56c5b67ff5989db3e2c7cb2adcf05ab73a1b
naitelkadir <naitelkadir@gmail.com>
2023-04-04 07:15:30 -0700

    fix bettu doc

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
-/*
- * listint_len - Returns the number of elements in a linked list
- * @h: Linked list
- *
- * Return: The number of nodes
+/**
+ * listint_len - function to print all elements
+ * @h: listint_t type of node struct
+ * Return: Always successful
  */
+
 size_t listint_len(const listint_t *h)
 {
 size_t c = 0;
