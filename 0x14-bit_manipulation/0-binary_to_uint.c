07deb84cd20f2ca2b26e12d557173403242f90ab
naitelkadir <naitelkadir@gmail.com>
2023-04-07 15:52:46 -0700

    dd task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - ...
+ * @b: pointer to string to be converted
+ *
+ * Return: converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int len = 0;
+	unsigned int i;
+	unsigned int decimal = 0;
+	
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (b[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (b[i] != '1' && b[i] != '0')
+		{
+			return (0);
+		}
+		decimal = decimal + (b[i] - '0') * pow(2, len - 1 - i);
+	}
+	return (decimal);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,8 @@
+0. 0
+Write a function that converts a binary number to an unsigned int
+
+1. 1
+Write a function that prints the binary representation of a number.
+
+2. 10
+Write a function that returns the value of a bit at a given index.
