a9b17ba5b100e55028bbc4e50387a334fdabddbe
naitelkadir <naitelkadir@gmail.com>
2023-04-07 16:39:46 -0700

    up

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -24,7 +24,7 @@ unsigned int binary_to_uint(const char *b)
 	}
 	for (i = 0; i < len; i++)
 	{
-		if (b[len - i] != 48 && b[len - i] != 49)
+		if (b[i] != 48 && b[i] != 49)
 		{
 			return (0);
 		}
