088cea8ecb1ab850791dc5b14523960ab84f0ba5
naitelkadir <naitelkadir@gmail.com>
2023-04-07 16:48:53 -0700

    add task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * set_bit - ...
+ * @n: ...
+ *@index: ...
+ *
+ * Return: ...
+ */
+int set_bit (unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	int clearbit = ~(1<<index);
+	int mask = *n & clearbit;
+	*n = mask |(1<<index);
+	return (1);
+}
