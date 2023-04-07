b7ba7f491cabfac4af51bf84c2b02138b265d927
naitelkadir <naitelkadir@gmail.com>
2023-04-07 16:56:21 -0700

    test

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,7 @@
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+void convert(unsigned long int bin);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
