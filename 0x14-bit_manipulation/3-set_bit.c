1c431b391bf16286ab7be4ee3865ecfa77c9e1aa
naitelkadir <naitelkadir@gmail.com>
2023-04-07 17:03:35 -0700

    up

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,12 +11,15 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int) * 8)
+	if (index > sizeof(n) * 8)
 	{
 		return (-1);
 	}
-	int clearbit = ~(1 << index);
-	int mask = *n & clearbit;
-	*n = mask |(1 << index);
-	return (1);
+	else
+	{
+		int clearbit = ~(1 << index);
+		int mask = *n & clearbit;
+		*n = mask |(1 << index);
+		return (1);
+	}
 }
