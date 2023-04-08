d2866b7da583202d87d0df95e8a50b55ebbffb1a
naitelkadir <naitelkadir@gmail.com>
2023-04-07 17:16:29 -0700

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -15,8 +15,11 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	{
 		return (-1);
 	}
-	int clearbit = ~(1<<index);
-	int mask = *n & clearbit;
-	*n = mask |(0<<index);
-	return (1);
+	else
+	{
+		int clearbit = ~(1<<index);
+		int mask = *n & clearbit;
+		*n = mask |(0<<index);
+		return (1);
+	}
 }
