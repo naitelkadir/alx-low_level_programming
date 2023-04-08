b984ca266212e01f1a7e828599d6122c30973262
naitelkadir <naitelkadir@gmail.com>
2023-04-07 17:13:14 -0700

    add task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * clear_bit - ...
+ * @n: ...
+ * @index: ...
+ *
+ * Return: ...
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+	{
+		return (-1);
+	}
+	int clearbit = ~(1<<index);
+	int mask = *n & clearbit;
+	*n = mask |(0<<index);
+	return (1);
+}
