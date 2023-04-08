60df5b4c83e484d5e15c3b647a2d4159044d9534
naitelkadir <naitelkadir@gmail.com>
2023-04-08 09:05:38 -0700

    up

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -20,11 +20,11 @@ int get_bit(unsigned long int n, unsigned int index)
 		{
 			if (n % 2 == 1)
 			{
-				return (1);
+				return (0);
 			}
 			else
 			{
-				return (0);
+				return (1);
 			}
 		}
 		n = n / 2;
