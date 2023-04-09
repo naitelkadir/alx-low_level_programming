3459d4bd6962d69d97567b61ef35f5c8114213e1
naitelkadir <naitelkadir@gmail.com>
2023-04-09 16:38:08 -0700

    add task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * read_textfile - ...
+ * @filename: ...
+ * @letters: ...
+ *
+ *Return: ...
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp, count;
+	char *b = malloc(sizeof(char *) * letters);
+	
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	if (b == NULL)
+	{
+		return (0);
+	}
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
+	{
+		return (0);
+	}
+	count = read(fd, b, letters);
+	write(STDOUT-FILENO, b, count);
+	close(fp);
+	return( count);
+}
