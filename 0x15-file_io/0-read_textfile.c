2fa0839433c34b7e0aa2e44279269e6e26305cf2
naitelkadir <naitelkadir@gmail.com>
2023-04-09 16:52:24 -0700

    test

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp, count;
+	int fp, count, fpwrite;
 	char *b = malloc(sizeof(char *) * letters);
 	
 	if (filename == NULL)
@@ -20,13 +20,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	fp = open(filename, O_RDONLY, 0600);
+	fp = open(filename, O_RDONLY);
 	if (fp == -1)
 	{
 		return (0);
 	}
 	count = read(fp, b, letters);
-	write(STDOUT-FILENO, b, count);
+	if (count == -1)
+	{
+		return (0);
+	}
+	fpwrite = write(STDOUT-FILENO, b, count);
+	if (fpwrite == -1)
+	{
+		return (0);
+	}
 	close(fp);
 	return (count);
 }
