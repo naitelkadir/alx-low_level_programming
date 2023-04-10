b8615c19dad89be62c36706dcddc852a03942625
naitelkadir <naitelkadir@gmail.com>
2023-04-10 03:11:34 -0700

    add permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	fp = open(filename, O_CREAT | O_TRUNC | O_WRONLY);
+	fp = open(filename, O_CREAT | O_TRUNC | O_WRONLY, rw-------);
 	if (fp == -1)
 	{
 		return (-1);
