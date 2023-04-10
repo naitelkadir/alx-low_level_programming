fbb7f69fac2ba73891cc555e3a9cf9c9201dfd6c
naitelkadir <naitelkadir@gmail.com>
2023-04-10 03:13:08 -0700

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	fp = open(filename, O_CREAT | O_TRUNC | O_WRONLY, rw-------);
+	fp = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fp == -1)
 	{
 		return (-1);
