7f5e1e67542683ed17f5d9e5854da18e1ff8cc02
naitelkadir <naitelkadir@gmail.com>
2023-04-10 17:09:30 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,13 +18,13 @@ int main(int argc,char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprinttf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd_to = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	do {
@@ -41,10 +41,15 @@ int main(int argc,char *argv[])
 			exit(99);
 		}
 	} while (fd_read == 1024)
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error:Can't close fd %d\n", fd_from);
+		exit(100);
+	}
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error:Can't close fd %d\n", fd_to);
-		exit(100);
+                exit(100);
 	}
 	return (0);
 }
