690b86aa6cb54e2749e10c74a7ac3c7da97b56ec
naitelkadir <naitelkadir@gmail.com>
2023-05-18 06:05:43 -0700

    add task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - ...
+ * @head: ...
+ * Return: ...
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp  = head->next;
+		free(head);
+		head = tmp;
+	}
+}
