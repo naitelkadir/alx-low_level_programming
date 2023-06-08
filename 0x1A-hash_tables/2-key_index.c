59431283c55a8db61e712315e6cb2876d017db25
naitelkadir <naitelkadir@gmail.com>
2023-06-08 06:03:42 -0700

    add task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - ...
+ * @key: ...
+ * @size: ...
+ * Return: ...
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	int index;
+	
+	index = hash_djb2(key);
+	return (index % size);
+}
