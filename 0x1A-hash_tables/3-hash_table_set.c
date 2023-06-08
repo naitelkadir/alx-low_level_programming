ed5130ffacf68caa3529400fa7168c1438a75677
naitelkadir <naitelkadir@gmail.com>
2023-06-08 11:18:55 -0700

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,9 +10,9 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index, i;
-	hash_node_t * new;
+	hash_node_t *new;
 	char *value_copy;
-	
+
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 	{
 		return (0);
